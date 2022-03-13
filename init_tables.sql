768e75c11d9f3030e6437f06707a0f296cc81225
Amelia Wibisono <ameliawibi@gmail.com>
2022-03-13 13:46:10 +0800

    create DB model

---
+++ init_tables.sql
@@ -0,0 +1,72 @@
+DROP TABLE IF EXISTS tasks_labels CASCADE;
+DROP TABLE IF EXISTS labels CASCADE;
+DROP TABLE IF EXISTS comments CASCADE;
+DROP TABLE IF EXISTS tasks CASCADE;
+DROP TABLE IF EXISTS task_statuses CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  avatar TEXT,
+  username TEXT,
+  user_email TEXT,
+  user_password TEXT
+);
+
+CREATE TABLE task_statuses (
+  id SERIAL PRIMARY KEY,
+  status TEXT
+);
+
+CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY,
+  due_date TEXT,
+  task_name TEXT,
+  description TEXT,
+  assigned_to INTEGER,
+  task_status INTEGER,
+  CONSTRAINT fk_tasks_status_id
+  FOREIGN KEY (task_status)
+  REFERENCES task_statuses(id)
+  ON DELETE CASCADE
+  ,
+  CONSTRAINT fk_tasks_user_id
+  FOREIGN KEY (assigned_to)
+  REFERENCES users(id)
+  ON DELETE CASCADE
+  );
+
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY,
+  comment TEXT,
+  task_id INTEGER,
+  user_id INTEGER,
+  CONSTRAINT fk_comments_task_id
+  FOREIGN KEY (task_id)
+  REFERENCES tasks(id)
+  ON DELETE CASCADE
+  ,
+  CONSTRAINT fk_comments_user_id
+  FOREIGN KEY (user_id)
+  REFERENCES users(id)
+  ON DELETE CASCADE
+  );
+
+  CREATE TABLE labels (
+  id SERIAL PRIMARY KEY,
+  label TEXT
+);
+
+CREATE TABLE tasks_labels (
+  id SERIAL PRIMARY KEY,
+  task_id INTEGER,
+  label_id INTEGER,
+  CONSTRAINT fk_tasklabel_task_id
+  FOREIGN KEY (task_id)
+  REFERENCES tasks(id)
+  ON DELETE CASCADE,
+  CONSTRAINT fk_tasklabel_label_id
+  FOREIGN KEY (label_id)
+  REFERENCES labels(id)
+  ON DELETE CASCADE
+);
