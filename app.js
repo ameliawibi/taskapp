41a4efd361dd3d8f009dd15202d4da420cbc3fb3
Amelia Wibisono <ameliawibi@gmail.com>
2022-03-14 21:51:58 +0800

    file upload

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import { taskRouter } from "./routes/task.js";
 import { authRouter } from "./routes/userAuth.js";
 import {router} from "./routes/index.js";
+import fileUpload from "express-fileupload";
 
 let app = express();
 
@@ -10,9 +11,10 @@ let app = express();
 app.set("view engine", "ejs");
 
 app.use(cookieParser());
-
+app.use(fileUpload());
 // serve static files
 app.use(express.static("uploads"));
+app.use(express.static("public"));
 
 app.use("/", router);
 app.use("/auth", authRouter);
--- controllers/userAuth.js
+++ controllers/userAuth.js
@@ -1,9 +1,34 @@
+import path from 'path';
+const __dirname = path.resolve();
+
 export const getSignup = (req,res) => {
-  res.send('NOT IMPLEMENTED: get sign up');
+  let ejsData = {
+    error: "",
+  }
+  res.render("signup",ejsData);
 };
 
 export const postSignup = (req,res) => {
-  res.send('NOT IMPLEMENTED: post sign up');
+  let avatar;
+  let uploadPath;
+  if (!req.files || Object.keys(req.files).length === 0) {
+    res.status(400).send('No files were uploaded.');
+    return;
+  }
+
+  console.log('req.files >>>', req.files); // eslint-disable-line
+
+  avatar = req.files.avatar;
+
+  uploadPath = __dirname + '/uploads/' + avatar.name;
+
+  avatar.mv(uploadPath, function(err) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    res.send('File uploaded to ' + uploadPath);
+  });
 };
 
 export const getLogin = (req,res) => {
