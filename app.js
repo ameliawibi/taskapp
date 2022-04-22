d50ffcf4643cd8c654d21eb5b2271406b35f3ce2
Amelia Wibisono <ameliawibi@gmail.com>
2022-04-22 19:58:58 +0800

    fix upper limit of payload

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ const io = new Server(httpServer);
 
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
-
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(cookieParser());
 app.use(fileUpload());
 // serve static files
